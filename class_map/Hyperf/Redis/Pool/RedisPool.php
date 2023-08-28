587bc83fef55f9239baa5721de71e984d438f87f
jmiy <751399695@qq.com>
2023-08-28 22:31:08 +0800

    重写  Hyperf\Redis\Pool\RedisPool::__construct  解决redis连接无法回收问题

---
+++ class_map/Hyperf/Redis/Pool/RedisPool.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Redis\Pool;
+
+use Hyperf\Collection\Arr;
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\Contract\ConnectionInterface;
+use Hyperf\Pool\Pool;
+use Hyperf\Redis\Frequency;
+use Hyperf\Redis\RedisConnection;
+use InvalidArgumentException;
+use Psr\Container\ContainerInterface;
+
+use function Hyperf\Support\make;
+
+class RedisPool extends Pool
+{
+    protected array $config;
+
+    public function __construct(ContainerInterface $container, protected string $name)
+    {
+        $config = $container->get(ConfigInterface::class);
+        $key = sprintf('redis.%s', $this->name);
+        if (!$config->has($key)) {
+            throw new InvalidArgumentException(sprintf('config[%s] is not exist!', $key));
+        }
+
+        $this->config = $config->get($key);
+        $options = Arr::get($this->config, 'pool', []);
+
+        $this->frequency = make(Frequency::class, ['pool' => $this]);
+
+        parent::__construct($container, $options);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    protected function createConnection(): ConnectionInterface
+    {
+        return new RedisConnection($this->container, $this, $this->config);
+    }
+}
